478342a9b66fea3084644ee3e0db6838c65e120d
weilikai01 <weilikai01@baidu.com>
2017-06-25 01:00:58 +0800

    fixed: 修复了离线识别功能在v3 sdk中的适配问题

--- sdk/android/eclipse/asr&wakeup/src/com/baidu/android/voicedemo/ActivityOffline.java
+++ sdk/android/eclipse/asr&wakeup/src/com/baidu/android/voicedemo/ActivityOffline.java
@@ -1,30 +1,26 @@
 package com.baidu.android.voicedemo;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.speech.RecognitionListener;
-import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import com.baidu.speech.VoiceRecognitionService;
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
 import com.baidu.speech.recognizerdemo.R;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class ActivityOffline extends Activity {
 
+    private static final String TAG = "ActivityLongSpeech";
     private TextView txtLog;
     private final String DESC_TEXT = "" +
             "离在线语法识别(首次使用需要联网授权)\n" +
@@ -39,6 +35,8 @@ public class ActivityOffline extends Activity {
             " 4. 明天天气怎么样(需要联网)\n" +
             " ..." +
             "\n";
+    private EventManager asr;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +45,62 @@ public class ActivityOffline extends Activity {
         findViewById(R.id.setting).setVisibility(View.GONE);
         findViewById(R.id.txtResult).setVisibility(View.GONE);
 
+        txtResult = (TextView) findViewById(R.id.txtResult);
         txtLog = (TextView) findViewById(R.id.txtLog);
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.baidu.action.RECOGNIZE_SPEECH");
-                intent.putExtra("grammar", "asset:///baidu_speech_grammar.bsg"); // 设置离线的授权文件(离线模块需要授权), 该语法可以用自定义语义工具生成, 链接http://yuyin.baidu.com/asr#m5
-                //intent.putExtra("slot-data", your slots); // 设置grammar中需要覆盖的词条,如联系人名
-                startActivityForResult(intent, 1);
-
-                txtLog.setText(DESC_TEXT);
+                start();
             }
         });
 
         txtLog.setText(DESC_TEXT);
+
+        // ########### 识别功能 ###########
+        // 1) 通过工厂创建语音识别的事件管理器
+        asr = EventManagerFactory.create(this, "asr");
+
+        // 2) 注册输出事件的监听器
+        asr.registerListener(new EventListener() {
+
+            @Override
+            public void onEvent(String name, String params, byte[] data, int offset, int length) {
+                print(name + ": " + params);
+                if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_READY)) {
+                    // 引擎就绪，可以说话，一般在收到此事件后通过UI通知用户可以说话了
+                }
+                if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+                    // 临时识别结果, 长语音模式需要从此消息中取出结果
+                    try {
+                        final JSONObject json = new JSONObject(params);
+                        final String result_type = json.getString("result_type");
+                        final String best_result = json.getJSONArray("results_recognition").getString(0);
+
+                        print(name + ": " + json.toString(4));
+                        txtResult.setText(best_result);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_FINISH)) {
+                    // 识别结束
+                    try {
+                        print(name + ": " + new JSONObject(params).toString());
+
+                        print("已经停止识别, 请检查日志确定停止原因。");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // ... 支持的输出事件和事件支持的事件参数见“输出事件”一节
+            }
+        });
+
+        HashMap intent = new HashMap();
+        bindParams(intent);
+        asr.send(SpeechConstant.ASR_KWS_LOAD_ENGINE, new JSONObject(intent).toString(), null, 0, 0);
     }
 
     @Override
@@ -74,5 +112,35 @@ public class ActivityOffline extends Activity {
             ArrayList<String> results_recognition = results.getStringArrayList("results_recognition");
             txtLog.append("识别结果(数组形式): " + results_recognition + "\n");
         }
+    }
+
+    public void bindParams(HashMap intent) {
+        intent.put("grammar", "asset:///baidu_speech_grammar.bsg"); // 设置离线的授权文件(离线模块需要授权), 该语法可以用自定义语义工具生成, 链接http://yuyin.baidu.com/asr#m5
+        //intent.putExtra("slot-data", your slots); // 设置grammar中需要覆盖的词条,如联系人名
+        intent.put("decoder", 2); // decoder 可以设置解码器类型, 2=离在线混合识别详见文档
+    }
+
+    private void start() {
+        HashMap intent = new HashMap();
+        bindParams(intent);
+
+        asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        asr.send(SpeechConstant.ASR_START, new JSONObject(intent).toString(), null, 0, 0);
+        try {
+            Log.d(TAG, "asr.start " + new JSONObject(intent).toString(4));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void cancel() {
+        asr.send("asr.cancel", "{}", null, 0, 0);
+    }
+
+    private void print(String msg) {
+        txtLog.append(msg + "\n");
+        ScrollView sv = (ScrollView) txtLog.getParent();
+        sv.smoothScrollTo(0, 1000000);
+        Log.d(TAG, "----" + msg);
     }
 }
